6ffb4813aaf1b4928f3833f30832997528878874
davleo <david.amadoral@gmail.com>
2023-05-15 23:39:15 -0500

    modified

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,20 +4,16 @@ import { throttle } from 'lodash';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-player.on(
-	'play',
-	throttle(async () => {
-		try {
-			const timeSeconds = await player.getCurrentTime();
-			localStorage.setItem('current-time', timeSeconds);
-		} catch (error) {
-			console.error(
-				'Error getting the current playing time of the video',
-				error
-			);
-		}
-	}, 1000)
-);
+const asynchronous = async () => {
+	try {
+		const timeSeconds = await player.getCurrentTime();
+		localStorage.setItem('current-time', timeSeconds);
+	} catch (error) {
+		console.error('Error getting the current playing time of the video', error);
+	}
+};
+
+player.on('play', throttle(asynchronous, 1000));
 
 const currentTime = localStorage.getItem('current-time');
 currentTime ? player.setCurrentTime(currentTime) : null;
